907b1c487a09fe3ba5f6c8dd694f5ecb753f5e1f
Nhu <vodanh5nhu@gmail.com>
2026-02-28 19:10:30 +0700

    Add Dockerfile for Render deployment

--- src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/config/DotenvConfig.java
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/config/DotenvConfig.java
@@ -1,13 +1,17 @@
 package vn.edu.hcmut.cse.adsoftweng.lab.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 
 @Configuration
+@Profile("!prod")
 public class DotenvConfig {
     
-    static {
+    @PostConstruct
+    public void loadEnv() {
         Dotenv dotenv = Dotenv.configure()
                 .ignoreIfMissing()
                 .load();
@@ -16,6 +20,6 @@ public class DotenvConfig {
             System.setProperty(entry.getKey(), entry.getValue())
         );
 
-        System.out.println("Da load thanh cong cac bien moi truong tu file .env");
+        System.out.println("Loaded .env file successfully!");
     }
 }
